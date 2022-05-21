cd76c97db9c1e69dd1704d2612cdd81084ff37ab
Islam00000 <96487054+Islam00000@users.noreply.github.com>
2022-05-21 14:44:42 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            scanner2 sc = new scanner2();
+            sc.Scanner("[ lowf 3num = 10.10 ] \n $$$ wlaaaaaaaaaa \n loopwhen { if { i == 3 } && { i != 67 } } \n" +
+              " /$ END $/ \n worthless ( iowf _new = 25.5 ; )");
+            Console.ReadKey();
+           
+        }
+    }
+}
